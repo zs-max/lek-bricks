3f3387e2176da43444ce687f358830cfa3e3dcac
lwk <lk_demo@163.com>
2025-08-17 21:16:43 +0800

    feat(补充对应组件代码文件)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
-import LText from './components/LText'
+import LText from '@/components/LText'
+import LImage from '@/components/LImage'
+import LShape from '@/components/LShape'
+import FinalPage from '@/components/FinalPage'
+
 import {App} from "vue";
-const components = [ 
+const components = [
   LText,
+  LImage,
+  LShape,
+  FinalPage
 ]
 
 const install = (app: App) => {
-  components.map((component:any) => {
+  components.map((component: any) => {
     app.use(component.name,component)
   })
 }
@@ -15,7 +22,10 @@ const install = (app: App) => {
  */
 export { 
   install,
-  LText
+  LText,
+  LImage,
+  LShape,
+  FinalPage
 }
 
 /**
